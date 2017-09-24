ea25d066334f1e348cc9c1841913bb05efb4e9b5
kavin <kavinmb2@gmail.com>
2017-09-24 21:01:26 +0530

    initial Commit

---
+++ src/com/learning/Main.java
@@ -0,0 +1,26 @@
+package com.learning;
+
+import com.learning.automobile.Car;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // chance to miss entering value - done
+        // chance to change the value - done
+        // repeated code for displaying - done
+        // cant change tyre count since its final - done
+        // updating wheel count has to update hasExtraWheel - done
+        // since hasExtraWheel is public it can be edited from out side
+
+        Car volvo = new Car(2, "v8", "Volvo", 4);
+        Car ferrari = new Car(2, "v8", "ferrari", 4);
+        Car bmw = new Car(2, "v8", "BMW", 4);
+
+        volvo.setTyreCount(5);
+
+        System.out.println(volvo);
+        System.out.println(ferrari);
+        System.out.println(bmw);
+    }
+}
---
+++ src/com/learning/automobile/Car.java
@@ -0,0 +1,55 @@
+package com.learning.automobile;
+
+/**
+ * Created by kavin on 24/09/17.
+ */
+public class Car {
+    private Integer tyreCount;
+    private String engineName;
+    private String vendor;
+    private Integer doorCount;
+    private Boolean hasExtraWheel;
+
+    public Car(Integer tyreCount, String engineName, String vendor, Integer doorCount) {
+        this.engineName = engineName;
+        this.vendor = vendor;
+        this.doorCount = doorCount;
+        this.setTyreCount(tyreCount);
+    }
+
+    public Integer getTyreCount() {
+        return tyreCount;
+    }
+
+    public void setTyreCount(Integer tyreCount) {
+        this.tyreCount = tyreCount;
+        this.hasExtraWheel = !(tyreCount > 0 && tyreCount <= 4);
+    }
+
+    public Boolean getHasExtraWheel() {
+        return hasExtraWheel;
+    }
+
+    public String getEngineName() {
+        return engineName;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public Integer getDoorCount() {
+        return doorCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "tyreCount=" + tyreCount +
+                ", engineName='" + engineName + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", doorCount=" + doorCount +
+                ", hasExtraWheel=" + hasExtraWheel +
+                '}';
+    }
+}
