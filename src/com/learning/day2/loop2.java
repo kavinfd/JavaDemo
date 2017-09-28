3bb2b0253253ca24e808c4e6babebaa20f95c893
kavin <kavinmb2@gmail.com>
2017-09-28 08:05:44 +0530

    loop 2

---
+++ src/com/learning/day2/loop2.java
@@ -0,0 +1,22 @@
+package com.learning.day2;
+
+/**
+ * Created by kavin on 28/09/17.
+ */
+public class loop2 {
+    static int i = 0;
+
+    public static void main(String args[]) {
+        for (; checkCondition(); incrementI()) {
+            System.out.println(i);
+        }
+    }
+
+    public static boolean checkCondition() {
+        return i < 10;
+    }
+
+    public static int incrementI() {
+        return i++;
+    }
+}
